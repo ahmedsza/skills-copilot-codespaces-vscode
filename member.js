2dbe1b44ee4f070574be3c9c32823b368949d1f7
Ahmed Salijee <37135205+ahmedsza@users.noreply.github.com>
2024-09-25 07:16:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {name: 'HTML5', level: 3},
+        {name: 'CSS3', level: 3},
+        {name: 'JavaScript', level: 2},
+        {name: 'AngularJS', level: 2},
+        {name: 'NodeJS', level: 2},
+        {name: 'MongoDB', level: 1}
+    ];
+}
